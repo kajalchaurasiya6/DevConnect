b51377b26858eff990f94c9c96a5f2524e08e410
kajal chaurasiya <chaurasiyak43@gmail.com>
2025-06-17 11:49:59 +0530

    fix: Fixed blink issue of  dark theme  on refresh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,25 @@
 import Layout from './components/layouts/Layout';
+import Login from './features/auth/Login';
 import './index.css';
 import HomePage from './pages/HomePage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-       <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route  path="/" key={'home'} element={<HomePage />} />
-          <Route  path="/profile" key={'profile'} element={<HomePage />} />
-          <Route  path="/settings" key={'settings'} element={<HomePage />} />
-          {/* Add more routes as needed */}
-        </Routes>
-      </Layout>
+ <BrowserRouter>
+      <Routes>
+        {/* Optional standalone route (like login) */}
+        <Route path="/" element={<Login />} />
+
+        {/* All routes that use Layout */}
+        <Route element={<Layout/>}>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile" element={<HomePage />} />
+          <Route path="/settings" element={<HomePage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
+
     </>
   )
 }
