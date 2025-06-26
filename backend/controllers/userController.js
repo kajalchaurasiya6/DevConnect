739080669080b226612847643ea496b14e96e4c2
kajal chaurasiya <chaurasiyak43@gmail.com>
2025-06-26 18:49:16 +0530

    backend/feat:Added validation in signup and login APIs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,8 +3,6 @@ const bcrypt = require('bcryptjs');
 exports.registerUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
-        // console.log(req.body)
-
         const userExists = await User.findOne({ email });
         if (userExists) return res.status(400).json({ message: 'User already exists' });
         const salt = await bcrypt.genSalt(10);
@@ -14,7 +12,6 @@ exports.registerUser = async (req, res) => {
             email,
             password: hashedPassword
         });
-        // console.log("user", user)
         res.status(201).json({
             _id: user._id,
             name: user.name,
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,26 @@ const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/userController');
 const { getMe, refreshToken, logout } = require('../controllers/authController');
 const { verifyAccessToken } = require('../middleware/authMiddleware');
+const validate = require('../middleware/validate');
+const { body } = require('express-validator');
 
 // ✅ Use only defined route handlers'
 
-router.post('/signup', registerUser);
-router.post('/login', loginUser);
+router.post('/signup',
+    [
+        body('name').notEmpty().withMessage('Name is required'),
+        body('email').isEmail().withMessage('Please enter a valid email'),
+        body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+    ],
+    validate,
+    registerUser);
+router.post('/login',
+    [
+        body('email').isEmail().withMessage('Enter a valid email'),
+        body('password').notEmpty().withMessage('Password is required'),
+    ],
+    validate,
+    loginUser);
 router.get('/me', verifyAccessToken, getMe);
 router.get('/refresh', refreshToken);
 router.post('/logout', logout);
